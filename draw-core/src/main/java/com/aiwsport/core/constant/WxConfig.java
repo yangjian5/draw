e1c6541e040ae8600df805b9e4090b5515722f86
yangjian9 <2389811519@qq.com>
2019-08-01 22:14:59 +0800

    iinit

--- draw-core/src/main/java/com/aiwsport/core/constant/WxConfig.java
+++ draw-core/src/main/java/com/aiwsport/core/constant/WxConfig.java
@@ -2,11 +2,11 @@ package com.aiwsport.core.constant;
 
 public class WxConfig {
     //小程序appid
-    public static final String appid = "wx169ddfe67114165d";
+    public static final String appid = "wx4fe0a3d3cc9cd26c";
     //微信支付的商户id
     public static final String mch_id = "123456";
     //微信支付的商户密钥
-    public static final String SECRET = "e26e1b29d8fc04e461d3277c919100aa";
+    public static final String SECRET = "65caf54750367c8efb6d9c3ab8ca4805";
     //支付成功后的服务器回调url
     public static final String notify_url = "https://dram.yj.com/api/wx_notify.json";
     //签名方式
